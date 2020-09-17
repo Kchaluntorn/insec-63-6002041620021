5801f511a0d34ea046896f5fe1fff2aa663f3a04
ชลันธร เกตุแก้ว <s6002041620021@kmutnb.ac.th>
2020-09-17 17:43:48 +0700

    add migrate of post

--- console/migrations/m200917_094640_create_post_table.php
+++ console/migrations/m200917_094640_create_post_table.php
@@ -15,9 +15,13 @@ class m200917_094640_create_post_table extends Migration
         $this->createTable('{{%post}}', [
             'id' => $this->primaryKey(),
             'title' => $this->string(250)->notNull(),
-            'short_desccirption' => $this->string(250)->notNull(),
+            'short_description' => $this->string(250)->notNull(),
             'description' => $this->text(),
-            'is_active' => $this->boolean()->defaultValue(0)
+            'is_active' => $this->boolean()->defaultValue(0),
+            'create_at' => $this->integer(),
+            'create_by' => $this->integer(),
+            'update_at' => $this->integer(),
+            'update_by' => $this->integer()
         ]);
     }
 
